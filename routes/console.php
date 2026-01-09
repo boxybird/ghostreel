adf6d0d2c340545e7fee446bbe1b8a16ce9762d7
boxybird <andrew.rhyand@gmail.com>
2026-01-08 17:57:21 -0700

    chore: remove unused `inspire` Artisan command from console routes

--- routes/console.php
+++ routes/console.php
@@ -6,10 +6,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function (): void {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
 /*
 |--------------------------------------------------------------------------
 | Movie Sync Schedules
